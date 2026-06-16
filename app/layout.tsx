841a6c57935ddf83e5c71c47e10f87950e6da71f
Raul Perez <raul.perezrios@gmail.com>
2026-06-15 20:55:59 -0600

    Agrega tag de Google Ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import LayoutClient from "./layout-client";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "OperLink",
@@ -19,6 +20,19 @@ export default function RootLayout({
     <html lang="es" className="h-full">
       <head>
         <script src="https://cdn.conekta.io/js/latest/conekta.js" async></script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-857113085"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-857113085');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col bg-white">
         <LayoutClient>{children}</LayoutClient>
